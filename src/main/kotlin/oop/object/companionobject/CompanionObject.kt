ff78e23c4ff04a89ef405a92a211b6e63e723d24
IkhsanMardika <ikhsancahyamardika@gmail.com>
2022-12-20 11:55:33 +0700

    delete

--- src/main/kotlin/oop/object/companionobject/CompanionObject.kt
+++
@@ -1,5 +0,0 @@
-package oop.`object`.companionobject
-
-fun main() {
-    println(Application.hello("Ikhsan"))
-}
--- src/main/kotlin/oop/object/singleton/SingletonObject.kt
+++
@@ -1,9 +0,0 @@
-package oop.`object`.singleton
-
-fun main(){
-    println(Utilities.name)
-    println(Utilities.toUpper("ikhsan"))
-
-    println(Application.Utilities.toUpper("dika"))
-
-}
